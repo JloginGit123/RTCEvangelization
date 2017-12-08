2f40fc6367a0cd52e601e962376d5134ae27b9c6
540302 <540302@CTSC00513942601.cts.com>
2017-12-08 12:56:18 +0530

    IssueId::UST13 entityType:AUSRST OwnerId::5555000209 Status::Done Reviewer::admin Comments::Committing code against UserStory

--- JavaProject/src/FirstClassFile.java
+++ JavaProject/src/FirstClassFile.java
@@ -4,7 +4,7 @@ public class FirstClassFile {
 	public static void main(String[] args)
 	{
 	
-		System.out.println("Hello World.RTC fix_DEF12 fix_second!_UST10_DEF21");
+		System.out.println("Hello World.RTC fix_DEF12 fix_second!_UST10_DEF21...");
 		
 	}
 
