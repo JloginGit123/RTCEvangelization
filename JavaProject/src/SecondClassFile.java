52561425903aec98f0a355962c2cb07da54d6ac7
540302 <540302@CTSC00513942601.cts.com>
2017-12-12 13:26:20 +0530

    IssueID::UST11 entityType::AUSRST OwnerId::5555000209 Reviewer::admin Status::Done Comments::Test commit against UST

---
+++ JavaProject/src/SecondClassFile.java
@@ -0,0 +1,11 @@
+package JavaProject.src;
+
+public class SecondClassFile
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("Hello World.RTC fix_DEF12 fix_second!_UST10_DEF21...");
+		
+	}
+}
